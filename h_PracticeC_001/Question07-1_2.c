be248f19c3ab7cf15e89c8e479f5d5678f6cbcd4
h.cho [h.cho] <h.cho@devcat.com>
2022-10-14 08:52:49 +0900

    문제풀이

---
+++ h_PracticeC_001/Question07-1_2.c
@@ -0,0 +1,18 @@
+//문제 2
+//프로그램 사용자로부터 양의 정수를 하나 입력받은 후 그 수만큼 3의 배수를 출력하는 프로그램 제작
+
+#include <stdio.h>
+
+int main(void)
+{
+    int num=0, cnt=0;
+    printf("3의 배수를 몇번 출력할까요? \n");
+    scanf("%d", &num);
+
+    while(cnt<num)
+    {
+        printf("%d", 3*cnt);
+        num++;
+    }
+    return 0;
+}
