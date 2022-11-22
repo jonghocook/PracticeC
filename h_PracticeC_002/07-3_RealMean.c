4a531541fea0f7f8ffc565155c8f76c921a9ca91
h.cho [h.cho] <h.cho@devcat.com>
2022-11-23 08:27:35 +0900

    커밋

---
+++ h_PracticeC_002/07-3_RealMean.c
@@ -0,0 +1,21 @@
+//예제
+
+#include <stdio.h>
+
+int main(void)
+{
+   int total=0;
+   int i, num;
+   printf("0부터 입력한 수 까지의 덧셈 : \n");
+   scanf("%d", &num);
+
+   for (i=0; i<=num; i++)
+    total+=i;
+
+    printf("0부터 %d 까지의 덧셈 결과 : %d \n", num, total);
+   
+   
+
+         
+    return 0;
+}
