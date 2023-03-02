dbf22d51a44a3d08fa49a92de8c9a16f926bd958
h.cho [h.cho] <h.cho@devcat.com>
2023-03-02 15:58:51 +0900

    test

---
+++ h_PracticeC_8/Practice_08_AdvanEnglishSchool.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main(void)
+{
+    char sel;
+    printf("M 오전, A 오후, E 저녁 \n");
+    printf(" 입력 : ");
+    scanf("%c", &sel);
+
+    switch(sel)
+    {
+    case 'M':
+    case 'm':
+    }
+
+    }
+    return 0;
+}
